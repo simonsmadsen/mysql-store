f6df8facdfe3cf2ffc57736c6c89823e2cfef3e8
Simon S Madsen <simon@smadsen.com>
2018-10-18 07:56:20 +0200

    asas

--- src/mysql/get-mysql-connection.js
+++ src/mysql/get-mysql-connection.js
@@ -8,16 +8,16 @@ export default async function getConnection(config) {
   if (!mysqlPool) {
     mysqlPool = pool.connectionPool(config)
     mysqlPool.on('acquire', (connection) => {
-      console.log('Connection %d acquired', connection.threadId)
+      // console.log('Connection %d acquired', connection.threadId)
     })
     mysqlPool.on('connection', (connection) => {
       connection.query('SET SESSION auto_increment_increment=1')
     })
     mysqlPool.on('enqueue', () => {
-      console.log('Waiting for available connection slot')
+      // console.log('Waiting for available connection slot')
     })
     mysqlPool.on('release', (connection) => {
-      console.log('Connection %d released', connection.threadId)
+      // console.log('Connection %d released', connection.threadId)
     })
   }
 
